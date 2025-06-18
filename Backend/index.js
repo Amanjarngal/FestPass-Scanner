1e4842c840a3099172bdac4fd18d273584c0f7e0
Amanjarngal <you@example.com>
2025-06-19 01:22:10 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -14,11 +14,11 @@ app.use(express.json());
 app.use("/api/register", registerRoute);
 app.use("/api/user", userRoute);
 
-mongoose
-  .connect("mongodb://localhost:27017/festDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("Mongo Error:", err));
 
