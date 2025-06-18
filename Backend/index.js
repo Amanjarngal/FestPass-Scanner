6ed778c8607db87efb00eb57ac7d323bc8adba99
Amanjarngal <you@example.com>
2025-06-19 02:03:22 +0530

    v

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ app.use("/api/user", userRoute);
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+
 })
 
   .then(() => console.log("MongoDB Connected"))
